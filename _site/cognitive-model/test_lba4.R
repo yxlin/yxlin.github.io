133d2d949b9b61fa4d474a5b406c5261cb706794
Yi-Shin Lin <yishinlin001@gmail.com>
2020-09-13 15:28:55 +0800

    correct lsm note

--- _site/cognitive-model/test_lba4.R
+++ _site/cognitive-model/test_lba4.R
@@ -2,40 +2,44 @@ cat("\n-------------------- Testing 4 Acc HLBA --------------------")
 rm(list = ls())
 ## setwd('/media/yslin/MERLIN/Documents/ggdmc/tests/testthat/Group2_LBA/')
 ## setwd('/Data/user_yslin/tests/testthat/Group2_LBA')
+## setwd('/home/yslin/yxlin.github.io-master/_docs/cognitive-model/')
 require(ggdmc)
 
+## 4 stimuli that have a shape and color (blue_diamond, blue_heart, green_diamond, and green_heart)
+## I assume a difficulty hierarchy (from easy to hard): green_diamond (e1) > green_heart (e2) > blue_diamond (e3) > blue_heart (e4). I also assume the easier stimulus, the higher its drift rate and the more variable its drift rate standard deviation. 
 model <- BuildModel(
-     p.map     = list(A = "1", B = "R", t0 = "1", mean_v = c("D", "M"),
-                      sd_v = "M", st0 = "1"),
-     ## BuildModel will convert 1, 2, 3, and 4 to "s1", "s2", "s3", and "s4".
-     match.map = list(M = list(s1 = 1, s2 = 2, s3 = 3, s4 = 4)),
-     factors   = list(S = c("s1", "s2", "s3", "s4"), D = c("d1", "d2", "d3", "d4")),
-     constants = c(sd_v.false = 1, st0 = 0),
-     responses = c("r1", "r2", "r3", "r4"),
-     type      = "norm")
+    p.map     = list(A = "1", B = "R", t0 = "1", mean_v = c("S", "M"),
+                     sd_v = "M", st0 = "1"),
+    match.map = list(M = list("blue_diamond" = "BD", "blue_heart" = "BH",
+                              "green_diamond" = "GD", "green_heart" = "GH")),
+    factors   = list(S = c("blue_diamond", "blue_heart", "green_diamond", "green_heart")),
+    constants = c(sd_v.false = 1, st0 = 0),
+    responses = c("BD", "BH", "GD", "GH"),
+    type      = "norm")
+model@npar
 
-pop.mean <- c(A=.4, B.r1=.5, B.r2=.6, B.r3=.7, B.r4=.8,
+pop.mean <- c(A=.4, B.BD=.5, B.BH=.6, B.GD=.7, B.GH=.8,
               t0=.3,
-              mean_v.d1.true  = 2.5,
-              mean_v.d2.true  = 2.0,
-              mean_v.d3.true  = 1.5,
-              mean_v.d4.true  = 1.0,
-              mean_v.d1.false = .15,
-              mean_v.d2.false = .20,
-              mean_v.d3.false = .25,
-              mean_v.d4.false = .30,
+              mean_v.blue_diamond.true   = 1.5,
+              mean_v.blue_heart.true     = 1.0,
+              mean_v.green_diamond.true  = 2.5,
+              mean_v.green_heart.true    = 2.0,
+              mean_v.blue_diamond.false  = .20,
+              mean_v.blue_heart.false    = .25,
+              mean_v.green_diamond.false = .10,
+              mean_v.green_heart.false   = .15,
               sd_v.true = .25)
 
-pop.scale <-c(A=.1, B.r1=.1, B.r2=.1, B.r3=.1, B.r4=.1,
+pop.scale <-c(A=.1, B.BD=.1, B.BH=.1, B.GD=.1, B.GH=.1,
               t0=.05,
-              mean_v.d1.true  =.2,
-              mean_v.d2.true  =.2,
-              mean_v.d3.true  =.2,
-              mean_v.d4.true  =.2,
-              mean_v.d1.false =.2,
-              mean_v.d2.false =.2,
-              mean_v.d3.false =.2,
-              mean_v.d4.false =.2,
+              mean_v.blue_diamond.true   = .2,
+              mean_v.blue_heart.true     = .2,
+              mean_v.green_diamond.true  = .2,
+              mean_v.green_heart.true    = .2,
+              mean_v.blue_diamond.false  = .2,
+              mean_v.blue_heart.false    = .2,
+              mean_v.green_diamond.false = .2,
+              mean_v.green_heart.false   = .2,
               sd_v.true = .1)
 
 pop.prior <- BuildPrior(
@@ -47,7 +51,7 @@ pop.prior <- BuildPrior(
 
 ## plot(pop.prior)
 ## Simulate some data ----------
-dat <- simulate(model, nsub = 12, nsim = 50, prior = pop.prior)
+dat <- simulate(model, nsub = 12, nsim = 30, prior = pop.prior)
 dmi <- BuildDMI(dat, model)
 ps <- attr(dat, "parameters")
 
@@ -65,26 +69,25 @@ mu.prior <- BuildPrior(
     lower = c(0,0,0,0,0,       .05, NA,NA,NA,NA, NA,NA,NA,NA, 0),
     upper = c(NA,NA,NA,NA,NA,    1, NA,NA,NA,NA, NA,NA,NA,NA, NA))
 
-## plot(p.prior, ps=ps)
-## plot(mu.prior, ps = pop.mean)
+plot(p.prior, ps=ps)
+plot(mu.prior, ps = pop.mean)
 
 sigma.prior <- BuildPrior(
     dists = rep("unif", model@npar),
-    p1    = c(A = 0, B.r1 = 0, B.r2 = 0, B.r3=0, B.r4=0,
+    p1    = c(A = 0, B.BD = 0, B.BH = 0, B.GD=0, B.GH=0,
               t0 = 0,
-              mean_v.d1.true = 0,
-              mean_v.d2.true = 0,
-              mean_v.d3.true = 0,
-              mean_v.d4.true = 0,
-              mean_v.d1.false = 0,
-              mean_v.d2.false = 0,
-              mean_v.d3.false = 0,
-              mean_v.d4.false = 0,
+              mean_v.blue_diamond.true   = 0,
+              mean_v.blue_heart.true     = 0,
+              mean_v.green_diamond.true  = 0,
+              mean_v.green_heart.true    = 0,
+              mean_v.blue_diamond.false  = 0,
+              mean_v.blue_heart.false    = 0,
+              mean_v.green_diamond.false = 0,
+              mean_v.green_heart.false   = 0,
               sd_v.true = 0),
     p2    = rep(5, model@npar))
 
 ## plot(sigma.prior, ps=pop.scale)
-
 ## Sampling -------------
 priors <- list(pprior=p.prior, location=mu.prior, scale=sigma.prior)
 
@@ -116,6 +119,8 @@ save(fit0, fit, model, pop.mean, pop.scale, pop.prior, dat, dmi, ps, p.prior,
      mu.prior, sigma.prior, priors, file = "data/LBA4A.RData")
 
 fit  <- run(fit, thin = 12, ncore = 6)
+save(fit0, fit, model, pop.mean, pop.scale, pop.prior, dat, dmi, ps, p.prior,
+     mu.prior, sigma.prior, priors, file = "data/LBA4A.RData")
 
 load('data/LBA4A.RData')
 plot(fit)
@@ -133,10 +138,14 @@ res <- gelman(fit, verbose = TRUE, subchain=9:12)
 ## Mean    8    5    2    6   12    9    7   10    3    4    1   11
 ## 1.54 1.10 1.23 1.25 1.25 1.29 1.35 1.47 1.48 1.54 1.73 1.78 2.97
 
+## Mean    8    5    2    6   12    7   10    3    4    9    1   11 
+## 1.33 1.06 1.14 1.15 1.18 1.26 1.26 1.31 1.34 1.35 1.36 1.59 1.94 
+## Mean    8    5    2    6   10   12    7    3    9    4    1   11 
+## 1.25 1.04 1.12 1.13 1.13 1.17 1.18 1.24 1.30 1.30 1.34 1.47 1.63
 
 load('data/LBA4A.RData'); ls()
-
 est0 <- summary(fit, recovery = TRUE, ps = ps, verbose =TRUE)
+
 ##            A    B.r1    B.r2    B.r3    B.r4      t0 mean_v.d1.true
 ## Mean  0.3652  0.5590  0.6447  0.8768  0.9125  0.2845         2.7431
 ## True  0.3668  0.4815  0.5696  0.7796  0.8150  0.2962         2.5801
@@ -164,3 +173,8 @@ est0 <- summary(fit, recovery = TRUE, ps = ps, verbose =TRUE)
 ## True          0.1943          0.1948          0.1579    0.1037
 ## Diff         -0.0945         -0.0724         -0.0445   -0.0193
 
+load('data/HLBA4A.RData'); ls()
+plot(fit)
+
+176612.76  / 60 / 60
+49/24
